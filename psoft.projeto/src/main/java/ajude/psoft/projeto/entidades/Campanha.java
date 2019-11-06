42bce543bb3e7e4c6883e8c9fcbc3cb8f0fd7b04
natansouzaa <natan.souza@ccc.ufcg.edu.br>
2019-11-06 17:53:13 -0300

    atualizacoes no lab

--- psoft.projeto/src/main/java/ajude/psoft/projeto/entidades/Campanha.java
+++ psoft.projeto/src/main/java/ajude/psoft/projeto/entidades/Campanha.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Campanha {
@@ -19,17 +20,20 @@ public class Campanha {
     private String status;
     private float meta;
     private String[] doacoes;
-    private String usuarioDono;
+    @ManyToOne
+    private Usuario usuarioDono;
     private String[] comentarios;
     private int curtidas;
 
 
     public Campanha() {
         super();
+        this.status = "ativa";
+        this.curtidas = 0;
     }
 
     public Campanha(String nomeCurto, String identificadorURL, String descricao,
-        Calendar dataLimite, String status, float meta, String[] doacoes, String usuarioDono,
+        Calendar dataLimite, String status, float meta, String[] doacoes, Usuario usuarioDono,
         String[] comentarios, int curtidas) {
         super();
         this.nomeCurto = nomeCurto;
@@ -45,7 +49,7 @@ public class Campanha {
     }
 
     public Campanha(long id, String nomeCurto, String identificadorURL, String descricao,
-        Calendar dataLimite, String status, float meta, String[] doacoes, String usuarioDono,
+        Calendar dataLimite, String status, float meta, String[] doacoes, Usuario usuarioDono,
         String[] comentarios, int curtidas) {
         super();
         this.id = id;
@@ -65,44 +69,88 @@ public class Campanha {
         return this.id;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public String getNomeCurto() {
         return this.nomeCurto;
+    }
+
+    public void setNomeCurto(String nomeCurto) {
+        this.nomeCurto = nomeCurto;
     }
 
     public String getIdentificadorURL() {
         return this.identificadorURL;
     }
 
+    public void setIdentificadorURL(String identificadorURL) {
+        this.identificadorURL = identificadorURL;
+    }
+
     public String getDescricao() {
         return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 
     public Calendar getDataLimite() {
         return this.dataLimite;
     }
 
+    public void setDataLimite(Calendar dataLimite) {
+        this.dataLimite = dataLimite;
+    }
+
     public String getStatus() {
         return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     public float getMeta() {
         return this.meta;
     }
 
+    public void setMeta(float meta) {
+        this.meta = meta;
+    }
+
     public String[] getDoacoes() {
         return this.doacoes;
     }
 
-    public String getUsuarioDono() {
+    public void setDoacoes(String[] doacoes) {
+        this.doacoes = doacoes;
+    }
+
+    public Usuario getUsuarioDono() {
         return this.usuarioDono;
+    }
+
+    public void setUsuarioDono(Usuario usuarioDono) {
+        this.usuarioDono = usuarioDono;
     }
 
     public String[] getComentarios() {
         return this.comentarios;
     }
 
+    public void setComentarios(String[] comentarios) {
+        this.comentarios = comentarios;
+    }
+
     public int getCurtidas() {
         return this.curtidas;
+    }
+
+    public void setCurtidas(int curtidas) {
+        this.curtidas = curtidas;
     }
 
 }
