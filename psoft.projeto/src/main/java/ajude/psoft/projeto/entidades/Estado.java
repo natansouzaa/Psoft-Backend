62d480d8e22eaff8d0fdc5e11cc64950c2f269bd
natansouzaa <natan.souza@ccc.ufcg.edu.br>
2019-11-27 16:53:59 -0300

    removendo a pasta que nao servia para o heroku

--- psoft.projeto/src/main/java/ajude/psoft/projeto/entidades/Estado.java
+++
@@ -1,7 +0,0 @@
-package ajude.psoft.projeto.entidades;
-
-public enum Estado {
-
-    ATIVA, ENCERRADA, VENCIDA, CONCLUIDA
-
-}
--- psoft.projeto/src/main/java/ajude/psoft/projeto/ordenacoes/Comparadores.java
+++
@@ -1,7 +0,0 @@
-package ajude.psoft.projeto.ordenacoes;
-
-import java.util.Comparator;
-
-public interface Comparadores extends Comparator<Object>{
-
-}
